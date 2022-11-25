a35afa32e1364d3703431b70a1bd2492787709a9
s991116 <philip.juhl@gmail.com>
2022-11-25 15:27:33 +0100

    Added test in list and Readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+#Unittest exercise
+
+#Run unittest
+To run unittests run the Command line
+python -m unittest -v
+
+#Unittest library
+Name Python file with unittest test_*
+Test method is named "test_xxx"
+Use the Arrange, Act, Assert for each test
--- test_Library.py
+++ test_Library.py
@@ -13,5 +13,15 @@ class Test_Library(unittest.TestCase):
     #Assert
     self.assertEqual(result, 5)
 
+  def test_3_in_array_1_to_5(self):
+    #Arrange
+    testArray = [1,2,3,4,5]
+
+    #Act
+
+    #Assert
+    self.assertIn(3, testArray)
+
+
 if __name__ == '__main__':
     unittest.main()
